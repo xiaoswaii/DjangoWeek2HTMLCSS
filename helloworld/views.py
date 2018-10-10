66811694929ceda02cb67d8c0250d48568a73d65
xiaoswaii <b05106028@ntu.edu.tw>
2018-10-11 01:21:35 +0800

    wk4

--- helloworld/views.py
+++ helloworld/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate
 from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from guestbook.models import TextMessage,Day
+from guestbook.models import TextMessage
 def index(request):
 
 	talk1 = TextMessage.objects.create(talker='Xiaoswaii',message='Hello,littleyang!' )
