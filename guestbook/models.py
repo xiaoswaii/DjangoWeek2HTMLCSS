039e5ccf37e462d614532b8d07417cb272726702
xiaoswaii <b05106028@ntu.edu.tw>
2018-10-19 02:05:26 +0800

    wk5

--- guestbook/models.py
+++ guestbook/models.py
@@ -4,8 +4,9 @@ from django.conf import settings
 class Text(models.Model):
 	user=models.CharField(max_length=20, blank=False)
 	talk = models.CharField(max_length=50, blank=True)
+	date_time=models.DateTimeField()
 	def _str_(self):
-		return self.user + "" + self.talk
+		return self.user + "" + self.talk + "" + self.date_time
 
 #class Day(models.Model):
 #	libai=models.CharField(max_length=20,blank=False)
--- helloworld/views.py
+++ helloworld/views.py
@@ -4,6 +4,8 @@ from django.contrib import auth
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from guestbook.models import Text
+import datetime
+
 def index(request):
 	return render(request, 'index.html')
 
@@ -11,7 +13,8 @@ def guestbook(request):
 	if request.method=='POST':
 		user=request.POST['user']
 		talk=request.POST['talk']
-		conversa=Text.objects.create(user=user, talk=talk)
+		date_time=datetime.datetime.now()
+		conversation=Text.objects.create(user=user, talk=talk,date_time=date_time)
 		conversation=Text.objects.all()
 	conversation=Text.objects.all()
 	return render(request, 'guestbookver1.html',locals())
